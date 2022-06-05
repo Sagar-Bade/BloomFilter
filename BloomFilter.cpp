d45eb734d902bb7a23691f2e5f9b1014f2925ea3
Sagar Bade <67834863+Sagar-Bade@users.noreply.github.com>
2022-06-05 12:21:54 +0530

    Create BloomFilter.cpp

---
+++ BloomFilter.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+class BloomFilter{
+    
+    bool bitarray[50];
+    int arrSize;
+    
+    public:
+    
+BloomFilter(){
+    
+    bitarray[50] = { false };
+	arrSize = 50;
+}
+    
+    
+    
+int hashfunction1(string s)
+    {
+	int hash = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		hash = ((hash << 6) + (hash << 16) - hash) + s[i];
+		hash = hash % arrSize;
+	}
+	return hash;
+    }
+
+int hashfunction2(string s)
+    {
+	int hash = 30;
+	
+	for (int i = 0; i < s.size(); i++) {
+		hash = ((hash << 5) + hash) + s[i];
+		hash = hash % arrSize;
+	}
+	return hash;
+    }
+
+int hashfunction3(string s)
+    {
+	int hash = 1;
+	for (int i = 0; i < s.size(); i++)
+	{
+		hash = hash + pow(19, i) * s[i];
+		hash = hash % arrSize;
+	}
+	return hash % arrSize;
+    }
+
+
+int hashfunction4(string s)
+    {
+	int hash = 7;
+	for (int i = 0; i < s.size(); i++)
+	{
+		hash = (hash * 31 + s[i]) % arrSize;
+	}
+	return hash % arrSize;
+    }
+
+
+
+    
+bool lookup( string s)
+{
+	int a = hashfunction1(s);
+	int b = hashfunction2(s);
+	int c = hashfunction3(s);
+	int d = hashfunction4(s);
+
+	if (bitarray[a] && bitarray[b] && bitarray
+		&& bitarray[d])
+		return true;
+	else
+		return false;
+}
+
+
+void insert( string s)
+{
+
+	if (lookup(s))
+		cout << s << " is Probably already present" << endl;
+	else
+	{
+	int a = hashfunction1(s);
+	int b = hashfunction2(s);
+	int c = hashfunction3(s);
+	int d = hashfunction4(s);
+
+		bitarray[a] = true;
+		bitarray[b] = true;
+		bitarray[c] = true;
+		bitarray[d] = true;
+
+		cout << s << " inserted" << endl;
+	}
+}
+
+
+    
+};
+
+int main()
+{
+	
+	string testarray[33]
+		= { "abound", "abounds",	 "abundance",
+			"abundant", "accessible", "bloom",
+			"blossom", "bolster",	 "bonny",
+			"bonus", "bonuses",	 "coherent",
+			"cohesive", "colorful",	 "comely",
+			"comfort", "gems",		 "generosity",
+			"generous", "generously", "genial",
+			"bluff", "cheater",	 "hate",
+			"war",	 "humanity",	 "racism",
+			"hurt",	 "nuke",		 "gloomy",
+			"facebook", "sagar", "twitter" };
+			
+	BloomFilter b;
+    
+	for (int i = 0; i < 33; i++) {
+		b.insert(testarray[i]);
+	}
+	return 0;
+}
